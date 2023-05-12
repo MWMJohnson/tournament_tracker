a342540f526149a018057a5ac6ee394e2844c170
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-12 01:32:46 -0600

    Feat: add discgolfers data table structure.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_175326) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_063415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discgolfers", force: :cascade do |t|
+    t.string "name"
+    t.integer "rating"
+    t.boolean "pdga_member"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tournaments", force: :cascade do |t|
     t.string "name"
