3cfe6d842c57d101cf29b5bf851ff2d8212fd3cb
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-11 22:02:07 -0600

    Feature: added user story 2 commentary.

--- spec/features/tournaments/show_spec.rb
+++ spec/features/tournaments/show_spec.rb
@@ -1,3 +1,11 @@
+# [ ] done
+
+# User Story 2, Parent Show 
+
+# As a visitor
+# When I visit '/parents/:id'
+# Then I see the parent with that id including the parent's attributes
+# (data from each column that is on the parent table)
 require 'rails_helper'
 
 RSpec.describe 'the tournaments show page' do
