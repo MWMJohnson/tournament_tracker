62e2b40e4c59d7aff70747e6e7ca5653169e97a5
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-16 23:04:03 -0600

    Feat: add #edit,#update, and #private methods.

--- app/controllers/discgolfers_controller.rb
+++ app/controllers/discgolfers_controller.rb
@@ -7,4 +7,21 @@ class DiscgolfersController < ApplicationController
     @discgolfer = Discgolfer.find(params[:id])
   end
 
+  def edit
+    @discgolfer = Discgolfer.find(params[:id])
+    
+  end
+
+  def update 
+    discgolfer = Discgolfer.find(params[:id])
+    discgolfer.update(discgolfer_params)
+    discgolfer.save
+    redirect_to "/discgolfers/#{discgolfer.id}"
+  end
+
+  private
+  def discgolfer_params
+    params.permit(:name, :rating, :pdga_member)
+  end
+
 end
