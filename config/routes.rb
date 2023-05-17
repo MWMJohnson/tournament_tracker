7986cff95e108a9ddf74dc1f8c74f806626cb723
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-16 18:05:16 -0600

    Feat: add tournaments#edit route.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get '/tournaments', to: 'tournaments#index'
+  get "/tournaments/new", to: 'tournaments#new'
   get '/tournaments/:id', to: 'tournaments#show'
   get '/discgolfers', to: 'discgolfers#index'
   get '/discgolfers/:id', to: 'discgolfers#show'
   get "/tournaments/:tournament_id/discgolfers", to: 'tournament_discgolfers#index'
-  
+  post "/tournaments", to: 'tournaments#create'
+  get "/tournaments/:id/edit", to: "tournaments#edit"
 end
