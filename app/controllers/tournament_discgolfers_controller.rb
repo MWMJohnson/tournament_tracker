4ea158e761c34051467d9c54be0de6954cb39dcc
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-05-16 23:04:44 -0600

    Feat: add #new,#create, and #private methods.

--- app/controllers/tournament_discgolfers_controller.rb
+++ app/controllers/tournament_discgolfers_controller.rb
@@ -3,4 +3,20 @@ class TournamentDiscgolfersController < ApplicationController
     @tournament = Tournament.find(params[:tournament_id])
     @discgolfers = @tournament.discgolfers
   end
+
+  def new
+    @tournament = Tournament.find(params[:id])
+  end
+
+  def create
+    tournament = Tournament.find(params[:id])
+    @discgolfer = tournament.discgolfers.create(tournament_discgolfer_params)
+    redirect_to "/tournaments/#{tournament.id}/discgolfers"
+  end
+
+  private 
+  def tournament_discgolfer_params
+    params.permit(:name, :rating, :pdga_member)
+  end
+
 end
